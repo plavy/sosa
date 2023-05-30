6b02820bdb8afd83274f6ffab73cd7ceb74859a7
plavy <tinplavec@gmail.com>
2023-05-30 20:37:45 +0200

    test output as well

--- lab3/test_app.py
+++ lab3/test_app.py
@@ -1,20 +1,25 @@
 import unittest
 import math
+import io
+
 import app
 
+from unittest import TestCase
 from unittest.mock import patch
 
-class TestApp(unittest.TestCase):
+class TestApp(TestCase):
 
     def test_perform_division(self):
         opm = app.OperationsManager(7, 0)
         result = opm.perform_division()
         self.assertTrue(math.isnan(result))
 
+    @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', return_value='3.4')
-    def test_float_input(self, input): # pylint: disable=redefined-builtin,unused-argument
+    def test_float_input(self, input, output): # pylint: disable=redefined-builtin,unused-argument
         try:
             app.login_success()
+            self.assertEqual(output.getvalue(), "1.0\nResult:  3.4\n")
         except: # pylint: disable=bare-except
             self.fail()
 
