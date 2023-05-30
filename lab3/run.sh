6e2bceb0baa3e13c9391f54b732cc1e068ce351a
plavy <tinplavec@gmail.com>
2023-05-30 18:44:27 +0200

    redirect sterr

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ${PERFORM_TEST} == "unittest" ]]; then python -m unittest > /logs/$(date '+%F-%T')-unittest
-elif [[ ${PERFORM_TEST} == "bandit" ]]; then bandit -r . > /logs/$(date '+%F-%T')-bandit
+if [[ ${PERFORM_TEST} == "unittest" ]]; then python -m unittest &> /logs/$(date '+%F-%T')-unittest
+elif [[ ${PERFORM_TEST} == "bandit" ]]; then bandit -r . &> /logs/$(date '+%F-%T')-bandit
 else python app.py
 fi
